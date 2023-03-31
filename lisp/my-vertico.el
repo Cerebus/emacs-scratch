9ed7e52085b11e4f72989c0e9458d172fca8cbed
Timothy J. Miller <tmiller@mitre.org>
2023-03-31 13:47:02 -0500

    enable vertico and consul in all minibuffers

--- lisp/my-vertico.el
+++ lisp/my-vertico.el
@@ -4,8 +4,23 @@
 (require 'vertico)
 (require 'vertico-directory)
 
+(setq minibuffer-prompt-properties
+      '(read-only t cursor-intangible t face minibuffer-prompt))
+
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+(setq enable-recursive-minibuffers t)
+
 ;; Cycle back to top/bottom result when the edge is reached
 (customize-set-variable 'vertico-cycle t)
+
+(with-eval-after-load "consult"
+  (setq completion-in-region-function
+      (lambda (&rest args)
+        (apply (if vertico-mode
+                   #'consult-completion-in-region
+                 #'completion--in-region)
+               args))))
 
 (vertico-mode 1)
 
