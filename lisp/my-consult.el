8b310284a975af3b7656fdf6af8b49d9e8bc31c0
Timothy J. Miller <cerebus2@gmail.com>
2023-04-01 15:14:43 -0500

    consult: bind consult-line to C-s/C-r

--- lisp/my-consult.el
+++ lisp/my-consult.el
@@ -52,7 +52,10 @@
          ;; Minibuffer history
          :map minibuffer-local-map
          ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+         ("M-r" . consult-history)                ;; orig. previous-matching-history-element
+	 :map global-map
+	 ("C-s" . consult-line)
+	 ("C-r" . consult-line))
 
   ;; Enable automatic preview at point in the *Completions* buffer. This is
   ;; relevant when you use the default completion UI.
