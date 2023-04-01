d29b1f792e13068e2c45c01ad212bb8e42c11e7f
Timothy J. Miller <cerebus2@gmail.com>
2023-04-01 14:29:45 -0500

    org: disable electric-pair for ?<

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -1,2 +1,10 @@
+(add-to-list 'org-modules 'org-tempo t)
+
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (setq-local electric-pair-inhibit-predicate
+			`(lambda (c)
+			   (if (char-equal c ?<)
+			       t ,electric-pair-inhibit-predicate c)))))
 
 (provide 'my-org)
