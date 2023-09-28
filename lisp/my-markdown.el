f34b0cef70c89857976acbcfa8d96bea48b2d20b
Timothy J. Miller <tmiller@mitre.org>
2023-09-28 07:38:51 -0500

    fix(markdown): add visual and adaptive wrap

--- lisp/my-markdown.el
+++ lisp/my-markdown.el
@@ -1,4 +1,8 @@
 (unless (package-installed-p 'markdown-mode)
   (package-install 'markdown-mode))
 
+(add-hook 'markdown-mode-hook #'visual-line-mode)
+(add-hook 'markdown-mode-hook #'visual-fill-column-mode)
+(add-hook 'markdown-mode-hook #'adaptive-wrap-prefix-mode)
+
 (provide 'my-markdown)
