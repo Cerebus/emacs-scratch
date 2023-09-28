a70bf2a25a98dd0f2e08619bf403312541218a89
Timothy J. Miller <cerebus2@gmail.com>
2023-09-27 19:51:15 -0500

    feat: add ob-mermaid

--- lisp/my-plantuml.el
+++ lisp/my-plantuml.el
@@ -1,6 +1,10 @@
 (unless (package-installed-p 'plantuml-mode)
   (package-install 'plantuml-mode))
 
+;; mermaid
+(unless (package-installed-p 'ob-mermaid)
+  (package-install 'ob-mermaid))
+
 (add-to-list 'auto-mode-alist '("\\.p(lant)?uml\\'" . plantuml-mode))
 
 (customize-set-variable 'plantuml-jar-path "~/.local/lib/plantuml.jar")
@@ -10,8 +14,13 @@
   (if (not (file-exists-p plantuml-jar-path))
     (plantuml-download-jar)))
 
+(defun my-org-confirm-babel-evaluate (lang body)
+  (not (member lang '("plantuml" "mermaid"))))
+
 (with-eval-after-load 'org
-  (org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t)))
-  (setq org-plantuml-jar-path plantuml-jar-path))
+  (org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t) (mermaid . t) (scheme . t)))
+  (setq org-plantuml-jar-path plantuml-jar-path)
+  (setq org-confirm-babel-evaluate #'my-org-confirm-babel-evaluate))
+
 
 (provide 'my-plantuml)
