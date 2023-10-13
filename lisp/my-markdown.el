6f6cf0930aecb484fc5b21eaf321d4e0b8fe1f5c
Timothy J. Miller <tmiller@mitre.org>
2023-10-13 14:35:23 -0500

    feat(markdown): add embark region target for markdown fenced code blocks

--- lisp/my-markdown.el
+++ lisp/my-markdown.el
@@ -5,4 +5,19 @@
 (add-hook 'markdown-mode-hook #'visual-fill-column-mode)
 (add-hook 'markdown-mode-hook #'adaptive-wrap-prefix-mode)
 
+(defun my-markdown-fenced-code-block ()
+  "Target a region between fenced code block markers."
+  (save-excursion
+    (let* ((head (search-backward "```" nil t))
+	   (start (progn (forward-line)
+			 (point)))
+	   (tail (search-forward "```" nil t))
+	   (end (progn (beginning-of-line)
+		       (point))))
+      (when (and head tail)
+	`(region ,(buffer-substring start end) . (,start . ,end))))))
+
+(with-eval-after-load 'embark
+  (add-to-list 'embark-target-finders 'my-markdown-fenced-code-block))
+
 (provide 'my-markdown)
