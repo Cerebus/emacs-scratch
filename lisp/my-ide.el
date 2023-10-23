872de87aab83eafb61ed99afa6c1c12f9dc056d8
Timothy J. Miller <tmiller@mitre.org>
2023-10-23 10:06:23 -0500

    fix(ide): better eglot integration with polymode

--- lisp/my-ide.el
+++ lisp/my-ide.el
@@ -16,4 +16,11 @@
 
 (global-set-key (kbd "C-c t e") #'my/toggle-flymake-diagnostics-buffer)
 (global-auto-revert-mode t)
+
+;; Set polymode to dispatch LSPs stuff to the correct major mode indirect buffer.
+;; https://github.com/polymode/polymode/issues/305#issuecomment-1018700437
+(with-eval-after-load 'polymode
+    (add-to-list 'polymode-run-these-after-change-functions-in-other-buffers 'eglot--after-change)
+    (add-to-list 'polymode-run-these-before-change-functions-in-other-buffers 'eglot--before-change))
+
 (provide 'my-ide)
