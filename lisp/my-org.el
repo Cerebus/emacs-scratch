c065a08210d6270625e0537346dd8428f2424224
Timothy J. Miller <tmiller@mitre.org>
2023-11-27 07:44:02 -0600

    fix(org): remove < from eletric-pair

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -1,11 +1,9 @@
 (customize-set-variable 'org-modules '(ol-doi ol-w3m ol-bibtex ol-gnus ol-info ol-irc ol-mhe ol-rmail ol-eww org-tempo))
 
-(add-hook 'org-mode-hook
-	  (lambda ()
-	    (setq-local electric-pair-inhibit-predicate
-			`(lambda (c)
-			   (if (char-equal c ?<)
-			       t ,electric-pair-inhibit-predicate c)))))
+(add-hook 'org-mode-hook (lambda ()
+           (setq-local electric-pair-inhibit-predicate
+                   `(lambda (c)
+                  (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
 
 (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
 
