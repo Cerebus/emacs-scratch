cfce04577742cbec1b6cc6b661e7fdd27c84f7d2
Timothy J. Miller <tmiller@mitre.org>
2024-05-03 08:18:03 -0500

    fix(yaml): remove yaml-ts-mode from auto-mode-alist

--- lisp/my-yaml.el
+++ lisp/my-yaml.el
@@ -4,13 +4,14 @@
 
 (autoload 'yaml-mode "yaml-mode" "A Major mode for YAML" t)
 
-(add-to-list 'auto-mode-alist '("[^\\.][^j][^2]\\.ya?ml". yaml-mode))
+(add-to-list 'auto-mode-alist '("[^\\.][^j][^2]\\.ya?ml" . yaml-mode))
 
 (my-tree-sitter-init 'yaml 'yaml-mode 'yaml-mode '("https://github.com/ikatyang/tree-sitter-yaml"))
 
 (add-hook 'yaml-mode-hook #'eglot-ensure)
 
-; yaml-ts-mode is crap, and yaml-mode doesn't grok tree-sitter, so we make it so.
+;; yaml-ts-mode is (currently) crap, and yaml-mode doesn't grok tree-sitter, so we make it so.
+(setq auto-mode-alist (delete '("\\.ya?ml\\'" . yaml-ts-mode) auto-mode-alist))
 (add-hook 'yaml-mode-hook (lambda ()
 			    (treesit-parser-create 'yaml)
 			    (combobulate-mode)))
