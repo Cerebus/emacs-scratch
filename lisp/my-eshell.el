6a5f94c1487ad31757b6301b9aae69ab8d6f8001
Timothy J. Miller <tmiller@mitre.org>
2024-10-01 08:30:45 -0500

    feat(shell): redefine project-eshell to handle general use cases

--- lisp/my-eshell.el
+++ lisp/my-eshell.el
@@ -8,9 +8,27 @@
   (add-to-list 'eshell-modules-list 'eshell-rebind) ; really only care avout C-d
   )
 
+(defun my/do-buffer-eshell (buffer-dir)
+  "Start or switch to an EShell in BUFFER-DIR."
+  (let* ((default-directory (expand-file-name buffer-dir))
+	 (eshell-buffer-name "*eshell*")
+	 (eshell-buffer (get-buffer eshell-buffer-name)))
+    (if eshell-buffer
+	(pop-to-buffer eshell-buffer (bound-and-true-p display-comint-buffer-action))
+      (eshell))))
+
+(defun my/project-eshell (arg)
+  "Start or switch to an EShell in the current project.  With one
+\\[universal-argument], start in the current buffer's directory
+instead.  With multiple \\[universal-argument], start in the
+user's home directory."
+  (interactive "P")
+  (cond ((eq (prefix-numeric-value arg) 4) (my/do-buffer-eshell default-directory))
+	((> (prefix-numeric-value arg) 4) (my/do-buffer-eshell "~/"))
+	(t (project-eshell))))
 
 ;; Keys
-(global-set-key (kbd "C-c o e") #'eshell)
+(global-set-key (kbd "C-x p e") #'eshell)
 
 ;;(define-key 'my-open-things-key-map (kbd "t") #'vterm)
 
