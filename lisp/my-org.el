e3160d0b5ebee3181943531b08dc0e619de7f71e
Timothy Miller <cerebus2@gmail.com>
2024-11-17 12:34:11 -0600

    fix: more prettify org-mode

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -16,6 +16,14 @@
   :hook
   ((org-mode . org-modern-mode)
    (org-agenda-finalize . org-modern-agenda))
+  :custom
+  (org-auto-align-tags nil)
+  (org-tags-column 0)
+  (org-catch-invisible-edits 'show-and-error)
+  (org-special-ctrl-a/e t)
+  (org-insert-heading-respect-content t)
+  (org-hide-emphasis-markers t)
+  (org-pretty-entities t)
   :config
   (global-org-modern-mode))
 
